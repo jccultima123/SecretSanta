e8a1118a62d3628ee230dae9a4ecc663b254ff3b
Lynne Finnigan <lynne.finnigan@screenmedia.co.uk>
2018-11-22 23:53:06 +0000

    Initial styling

--- ss18/src/Home.js
+++ ss18/src/Home.js
@@ -1,84 +1,97 @@
 import React from 'react';
-import logo from './react.svg';
 import './Home.css';
 
 class Home extends React.Component {
-
-
   render() {
     return (
-      <div className="Home">
-        <button onClick={this.submit}>Submit</button>
-      </div>
+      <section className="l-content">
+        <div className="m-form">
+          <h1 className="e-title">Secret Santa</h1>
+          <p>Welcome to Laura's awesome secret santa app!</p>
+          <p>Add your details below and we'll email your results.</p>
+          <div className="e-formfield">
+            <input type="text" placeholder="Name" />
+          </div>
+          <div className="e-formfield">
+            <input type="email" placeholder="Email address" />
+          </div>
+          <div>
+            <button className="btn" onClick={this.submit}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </section>
     );
   }
 
   submit() {
-
     var fakeData = [
       {
-        "id": 0,
-        "name": "Jordan",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "Flat 209, 3 Cambridge Street, Chorlton Mill, Manchester M15BY",
-        "cantHave": [1]
+        id: 0,
+        name: 'Jordan',
+        emailAddress: 'dev@spun-labs.com',
+        address:
+          'Flat 209, 3 Cambridge Street, Chorlton Mill, Manchester M15BY',
+        cantHave: [1]
       },
       {
-        "id": 1,
-        "name": "Vanesa",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "Flat 209, 3 Cambridge Street, Chorlton Mill, Manchester M15BY",
-        "cantHave": [0]
+        id: 1,
+        name: 'Vanesa',
+        emailAddress: 'dev@spun-labs.com',
+        address:
+          'Flat 209, 3 Cambridge Street, Chorlton Mill, Manchester M15BY',
+        cantHave: [0]
       },
       {
-        "id": 2,
-        "name": "Laura",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "8 The Mount, Canterbury, Kent CT3 4AN",
-        "cantHave": []
+        id: 2,
+        name: 'Laura',
+        emailAddress: 'dev@spun-labs.com',
+        address: '8 The Mount, Canterbury, Kent CT3 4AN',
+        cantHave: []
       },
       {
-        "id": 3,
-        "name": "Olivia",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "100 Battersby Road, London SE6 1SB",
-        "cantHave": [4]
+        id: 3,
+        name: 'Olivia',
+        emailAddress: 'dev@spun-labs.com',
+        address: '100 Battersby Road, London SE6 1SB',
+        cantHave: [4]
       },
       {
-        "id": 4,
-        "name": "Tirth",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "100 Battersby Road, London SE6 1SB",
-        "cantHave": [3]
+        id: 4,
+        name: 'Tirth',
+        emailAddress: 'dev@spun-labs.com',
+        address: '100 Battersby Road, London SE6 1SB',
+        cantHave: [3]
       },
       {
-        "id": 5,
-        "name": "Carly",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "34 Todshaugh Gardens, Kirkliston EH29 9GE",
-        "cantHave": [6, 7]
+        id: 5,
+        name: 'Carly',
+        emailAddress: 'dev@spun-labs.com',
+        address: '34 Todshaugh Gardens, Kirkliston EH29 9GE',
+        cantHave: [6, 7]
       },
       {
-        "id": 6,
-        "name": "Michael",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "34 Todshaugh Gardens, Kirkliston EH29 9GE",
-        "cantHave": [5, 7]
+        id: 6,
+        name: 'Michael',
+        emailAddress: 'dev@spun-labs.com',
+        address: '34 Todshaugh Gardens, Kirkliston EH29 9GE',
+        cantHave: [5, 7]
       },
       {
-        "id": 7,
-        "name": "Liam",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "126 Plodder Lane, Bolton BL4 0BU",
-        "cantHave": [5, 6]
+        id: 7,
+        name: 'Liam',
+        emailAddress: 'dev@spun-labs.com',
+        address: '126 Plodder Lane, Bolton BL4 0BU',
+        cantHave: [5, 6]
       },
       {
-        "id": 8,
-        "name": "Harriet",
-        "emailAddress": "dev@spun-labs.com",
-        "address": "14/2 Henderson Street, Edinburgh, EH6 6BS",
-        "cantHave": []
-      },
+        id: 8,
+        name: 'Harriet',
+        emailAddress: 'dev@spun-labs.com',
+        address: '14/2 Henderson Street, Edinburgh, EH6 6BS',
+        cantHave: []
+      }
     ];
 
     var restrictedFirst = [];
@@ -100,14 +113,18 @@ class Home extends React.Component {
 
       //console.log("[already taken]", dataToSend.map(x => x.ss_id));
 
-      var cantHaves = thisGuy.cantHave.concat([thisGuy.id]).concat(dataToSend.map(x => x.ss_id));
+      var cantHaves = thisGuy.cantHave
+        .concat([thisGuy.id])
+        .concat(dataToSend.map(x => x.ss_id));
       //console.log("["+ thisGuy.name  +" can't have]", cantHaves);
 
-      var canHave = restrictedFirst.filter(function(person) {
-        return cantHaves.indexOf(person.id) === -1;
-      }).sort(function() {
-        return 0.5 - Math.random();
-      });
+      var canHave = restrictedFirst
+        .filter(function(person) {
+          return cantHaves.indexOf(person.id) === -1;
+        })
+        .sort(function() {
+          return 0.5 - Math.random();
+        });
 
       var recipient = canHave[0];
 
@@ -124,7 +141,10 @@ class Home extends React.Component {
             });
 
             //if they can have you, and you can have their person...
-            return person.cantHave.indexOf(thisGuy.id) === -1 && thisGuy.cantHave.indexOf(theirPerson.id) === -1;
+            return (
+              person.cantHave.indexOf(thisGuy.id) === -1 &&
+              thisGuy.cantHave.indexOf(theirPerson.id) === -1
+            );
           }
 
           //not in the data to send pile?
@@ -140,7 +160,10 @@ class Home extends React.Component {
           return peeps.id === sendingData.ss_id;
         });
 
-        console.log("[amendment for " + sendingData.to_name + "]", thisGuy.name);
+        console.log(
+          '[amendment for ' + sendingData.to_name + ']',
+          thisGuy.name
+        );
 
         //you are now their recipient
         sendingData.ss_id = thisGuy.id;
@@ -148,32 +171,32 @@ class Home extends React.Component {
         sendingData.ss_address = thisGuy.address;
       }
 
-      console.log("["+ thisGuy.name  +" has]", recipient.name);
+      console.log('[' + thisGuy.name + ' has]', recipient.name);
 
       dataToSend.push({
-        "id": thisGuy.id,
-        "to_name": thisGuy.name,
-        "to_email": thisGuy.emailAddress,
-        "ss_id": recipient.id,
-        "ss_name": recipient.name,
-        "ss_address": recipient.address
+        id: thisGuy.id,
+        to_name: thisGuy.name,
+        to_email: thisGuy.emailAddress,
+        ss_id: recipient.id,
+        ss_name: recipient.name,
+        ss_address: recipient.address
       });
 
       //console.log("[data to send]", dataToSend);
     }
 
-    console.log("[data to send]", dataToSend);
+    console.log('[data to send]', dataToSend);
 
-    for(var y = 0; y < dataToSend.length; y++) {
+    for (var y = 0; y < dataToSend.length; y++) {
       var emailParams = {
         service_id: 'sendgrid',
         template_id: 'template_fpKv8aBD',
         user_id: 'user_5WQaWmRYiqDIkifesP1Cz',
         template_params: {
-            'to_name': dataToSend[y].to_name,
-            'to_email': dataToSend[y].to_email,
-            'ss_name': dataToSend[y].ss_name,
-            'ss_address': dataToSend[y].ss_address
+          to_name: dataToSend[y].to_name,
+          to_email: dataToSend[y].to_email,
+          ss_name: dataToSend[y].ss_name,
+          ss_address: dataToSend[y].ss_address
         }
       };
 
